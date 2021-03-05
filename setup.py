9765e6afd6afe45d54c1abb457bd21b21787e264
electron001 <mbiskup123@gmail.com>
2021-03-05 12:32:00 +0000

    example

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ classifiers = ['Development Status :: 4 - Beta',
                'Operating System :: POSIX :: Linux',
                'License :: OSI Approved :: Apache Software License',
                'Intended Audience :: Developers',
-               'Programming Language :: Python :: 3.8',
+               'Programming Language :: Python :: 3.8.5',
                'Topic :: Software Development',
                'Topic :: System :: Hardware']
 
 setup(
     name         = 'lsm9ds1_python',
-    version      = '1.0.1',
+    version      = '1.0.2',
     author       = 'Maciej Biskup',
     author_email = 'mbiskup123@gmail.com',
     description  = "Library for lsm9ds1",
